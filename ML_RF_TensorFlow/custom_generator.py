76b8f654eacf43426f57a9c652fdbf4c313f1c4a
Nathaniel Smith <nathaniel-smith@hotmail.co.uk>
2022-09-15 17:04:23 +0200

    completed generator

--- ML_RF_TensorFlow/custom_generator.py
+++ ML_RF_TensorFlow/custom_generator.py
@@ -17,55 +17,62 @@ class data_generator(Sequence):
     # def __init__(self, list_IDs, labels, dim, batch_size=32, n_channels=1,
     #              n_classes=2, shuffle=True):
     
-    def __init__(self, x_set, y_set, dim, batch_size, n_channels=1,
-                 n_classes=2, shuffle=True):
+    def __init__(self, x_set, y_set, batch_size, shuffle=True):
         'Initialisation'
-        self.dim = dim
+        # self.dim = dim
         self.batch_size = batch_size
         self.x_data = x_set
         self.y_data = y_set
         # self.labels = labels
-        self.list_IDs = range(0, len(self.x_data))
-        self.n_channels = n_channels
-        self.n_classes = n_classes
+        self.datalen = len(y_set)
+        self.indexes = np.arange(self.datalen)
+
+        # self.list_IDs = range(0, len(self.x_data))
         self.shuffle = shuffle
         self.on_epoch_end()
 
     def __len__(self):
-        return int(np.ceil(len(self.list_IDs) / self.batch_size))
+        return int(np.ceil(self.datalen / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch'
         indexes = self.indexes[index*self.batch_size : (index+1)*self.batch_size]
 
-        tmp_list_IDs = [self.list_IDs[k] for k in indexes]
+        # tmp_list_IDs = [self.list_IDs[k] for k in indexes]
+
+        x_batch = self.x_data[indexes]
+        y_batch = self.y_data[indexes]
 
         #generate data
-        x, y = self.__generate_data(tmp_list_IDs)
+        # x, y = self.__generate_data(tmp_list_IDs)
 
-        return x, y
+        return x_batch, y_batch
 
     def on_epoch_end(self):
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(self.datalen)
 
         if self.shuffle:
             np.random.shuffle(self.indexes)
 
-    def __generate_data(self, tmp_list_IDs):
-        'generates the data in batch size samples'
+    # def __generate_data(self, tmp_list_IDs):
+    #     'generates the data in batch size samples'
 
-        X = np.empty((self.batch_size, self.dim, self.n_channels))
-        Y = np.empty((self.batch_size), dtype=int)
+    #     X = np.empty((self.batch_size, self.dim))
+    #     Y = np.empty((self.batch_size), dtype=int)
 
-        print(self.x_data[0].shape)
-        print(X[0].shape)
-        print(X.shape)
+    #     # print(self.x_data[0].shape)
+    #     # print(X[0].shape)
+    #     # print(X.shape)
 
-        for ID, i in enumerate(tmp_list_IDs):
-            X[ID,] = self.x_data[i]
-            Y[ID] = self.y_data[i]
+    #     for ID, i in enumerate(tmp_list_IDs):
+    #         X[ID] = self.x_data[i]
+    #         Y[ID] = self.y_data[i]
+            
+    #         print(self.x_data[i])
+    #         print(self.y_data[i])
+    #         break
 
 
-        return X, Y
+    #     return X, Y
 
 # generator = data_generator(ada_x_mmap, ada_y_mmap, dim, batch_size, n_classes=classes)
