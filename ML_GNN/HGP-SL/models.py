56e74c91e6f76aace0fc76a49bb560ea08118d91
Nathaniel Smith <nathaniel-smith@hotmail.co.uk>
2022-09-29 09:08:11 +0200

    HGP-SL model added

---
+++ ML_GNN/HGP-SL/models.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+from torch_geometric.nn import GCNConv
+
+from layers import GCN, HGPSLPool
+
+
+class Model(torch.nn.Module):
+    def __init__(self, num_features, n_hid, num_classes, pool_rat, drp_rat, smp_neigh, sprs_att, str_learn, lamb):
+        super(Model, self).__init__()
+        # self.args = args
+        self.num_features = num_features
+        self.nhid = n_hid
+        self.num_classes = num_classes
+        self.pooling_ratio = pool_rat
+        self.dropout_ratio = drp_rat
+        self.sample = smp_neigh
+        self.sparse = sprs_att
+        self.sl = str_learn
+        self.lamb = lamb
+
+        self.conv1 = GCNConv(self.num_features, self.nhid)
+        self.conv2 = GCN(self.nhid, self.nhid)
+        self.conv3 = GCN(self.nhid, self.nhid)
+
+        self.pool1 = HGPSLPool(self.nhid, self.pooling_ratio, self.sample, self.sparse, self.sl, self.lamb)
+        self.pool2 = HGPSLPool(self.nhid, self.pooling_ratio, self.sample, self.sparse, self.sl, self.lamb)
+
+        self.lin1 = torch.nn.Linear(self.nhid * 2, self.nhid)
+        self.lin2 = torch.nn.Linear(self.nhid, self.nhid // 2)
+        self.lin3 = torch.nn.Linear(self.nhid // 2, self.num_classes)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        edge_attr = None
+
+        x = F.relu(self.conv1(x, edge_index, edge_attr))
+        x, edge_index, edge_attr, batch = self.pool1(x, edge_index, edge_attr, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index, edge_attr))
+        x, edge_index, edge_attr, batch = self.pool2(x, edge_index, edge_attr, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index, edge_attr))
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(x1) + F.relu(x2) + F.relu(x3)
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.relu(self.lin2(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.log_softmax(self.lin3(x), dim=-1)
+
+        return x
